863564c02b8f3478e685fc5f6ac153e055d971cd
Urzhumtsev <urzhumtsev.max@gmail.com>
2018-10-04 15:57:40 +0300

    Переделана таблица кадидатов

--- exa1.sql
+++ exa1.sql
@@ -6,7 +6,7 @@ START TRANSACTION ;
 
 create schema if not exists politicians ;
 
-create table  politicians.vibory (
+/*create table politicians.vibory (
    num bigserial not null,
    vib_url text not null unique,
    vib_date date not null,
@@ -14,44 +14,20 @@ create table  politicians.vibory (
    vib_type int not null,
    vib_region int not null,
    constraint vibory_pkey primary key (vib_date, vib_name)
-) ;
+) ;*/
 
 
 create table politicians.candidates (
-   num bigserial not null,
-   vib_url text not null,
-   cand_url text not null unique,
-   cand_name text not null,
+   num bigserial not null primary key,
+   vib_url text references politicians.vibory (vib_url),
+   cand_url text not null,
+   cand_name text,
    birth_date date not null,
    party text not null,
    vidvizh text not null,
    registr text,
-   izbr text,
-   constraint candidates_pkey primary key (cand_name, birth_date),
-   constraint candidates_fkey
-      foreign key (vib_url)
-      references politicians.vibory(vib_url)
+   izbr text
 ) ;
-
-
-create or replace function
-politicians.raw_data ( avib_url text, 
-                       avib_date date, 
-                       avib_name text, 
-                       avib_type int ) 
-   returns text
-   language plpgsql
-   security definer
-   returns null on null input
-   volatile
-   set search_path = politicians, public
-   as
-$$
-BEGIN
-   INSERT INTO vibory (vib_url, vib_date, vib_name, vib_type) VALUES (avib_url, avib_date, avib_name, avib_type) ;
-   return 0 ;
-END ;
-$$ ;
 
 -- ROLLBACK TRANSACTION ;
 COMMIT TRANSACTION ;
